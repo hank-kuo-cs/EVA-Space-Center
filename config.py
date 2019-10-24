e3a33aa461d6b364c81961a71521b332528e6154
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2019-10-24 17:06:45 +0800

    fix data loading

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(m
 os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-DATASET_PATH = '~/dataset_random/'
+DATASET_PATH = os.path.expanduser('~') + '/dataset_random/'
 SPLIT_DATASET_SIZE = {'train': 10000, 'test': 10000, 'valid': 10000}
 DATASET_SIZE = {'train': 80000, 'test': 10000, 'validation': 10000}
 LOG_STEP = 100
