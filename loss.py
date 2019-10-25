1d465f370b758b9aaa894c528df151a0d542566e
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2019-10-25 10:38:25 +0800

    update MoonMSELoss

--- loss.py
+++ loss.py
@@ -1,6 +1,5 @@
 import numpy as np
 from config import *
-BATCH_SIZE = 2
 
 
 def get_error_percentage(output, target):
@@ -34,38 +33,22 @@ class MoonMSELoss(torch.nn.Module):
         super(MoonMSELoss, self).__init__()
 
     def forward(self, outputs, targets):
-        batch_circle_num = []
+        constant_penalties = []
 
         for i in range(BATCH_SIZE):
-            circle_num = np.array([0, 0], dtype=np.float64)
-
-            targets[i][1] /= 2 * np.pi
-            targets[i][2] /= np.pi
-
-            outputs[i][1] = torch.div(outputs[i][1], 2 * np.pi)
-            outputs[i][2] = torch.div(outputs[i][2], np.pi)
+            constant_penalty = np.array([0, 0], dtype=np.float64)
 
             for j in range(1, 3):
-                if outputs[i][j] < 0:
-                    circle_num[j - 1] = outputs[i][j] // -1
-                    outputs[i][j] = torch.remainder(outputs[i][j], 1)
+                constant_penalty[j - 1] = abs(outputs[i][j] // (2 * np.pi))
+                outputs[i][j] = torch.remainder(outputs[i][j], 2 * np.pi)
 
-                dis = abs(outputs[i][j] % 1 - targets[i][j])
-                if dis > 0.5:
-                    if outputs[i][j] % 1 < targets[i][j]:
-                        targets[i][j] = outputs[i][j] + 1 - dis
-                    else:
-                        targets[i][j] = outputs[i][j] - 1 + dis
-                circle_num[j - 1] += outputs[i][j] // 1
-                outputs[i][j] = torch.remainder(outputs[i][j], 1)
+                if abs(outputs[i][j] - targets[i][j]) > np.pi:
+                    targets[i][j] = 2 * np.pi + targets[i][j] if targets[i][j] < outputs[i][j] else -2 * np.pi + targets[i][j]
 
-            outputs[i][2] = torch.mul(outputs[i][2], 2)
-            circle_num[1] *= 2
+            constant_penalties.append(constant_penalty)
 
-            batch_circle_num.append(circle_num)
-
-        batch_circle_num = np.array(batch_circle_num).sum() / BATCH_SIZE / 2 / 100
-        amount_loss = torch.tensor(batch_circle_num, dtype=torch.double)
+        constant_penalties = np.array(constant_penalties).sum() / BATCH_SIZE / 2 / 10000
+        amount_loss = torch.tensor(constant_penalties, dtype=torch.double)
 
         mse_loss = torch.nn.MSELoss()(outputs, targets)
         loss = torch.add(mse_loss, amount_loss)
