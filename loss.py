d8ad8a575ad23b99da9e66649b91e7b2c7dd434a
charleschiu2012 <charleschiu2012@gmail.com>
2019-11-27 20:36:36 +0800

    fix test bug

--- loss.py
+++ loss.py
@@ -61,7 +61,7 @@ class BCMSELoss(torch.nn.Module):
             outputs[i][6: 9], scalar = get_scalar(outputs[i][6: 9])
 
         constant_penalties = (np.array(constant_penalties).sum() + scalar) / BATCH_SIZE / CONSTANT_WEIGHT
-        amount_loss = torch.tensor(constant_penalties, dtype=torch.double)
+        amount_loss = torch.tensor(constant_penalties.clone().detach(), dtype=torch.double)
 
         mse_loss = torch.nn.MSELoss()(outputs, targets)
         loss = torch.add(mse_loss, amount_loss)
