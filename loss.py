38594dd1590091f15a6f25095b055f43c4e2f57f
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-02 06:28:25 +0800

    Add CosSimiBCLoss

--- loss.py
+++ loss.py
@@ -147,8 +147,8 @@ class CosSimiBCLoss(torch.nn.Module):
                 constant_penalties += -1 * (targets_scalar - outputs_scalar)
                 print(torch.reshape(outputs[i][j:j + 3], (1, 3)))
                 similarity_loss += -1 * torch.nn.CosineSimilarity(dim=1, eps=1e-6)(
-                                            torch.reshape(outputs[i][j:j + 3], (1, 3)),
-                                            torch.reshape(targets[i][j:j + 3], (1, 3)))
+                                            torch.reshape(outputs[i][j:j + 3].cpu(), (1, 3)),
+                                            torch.reshape(targets[i][j:j + 3].cpu(), (1, 3)))
 
         constant_loss = torch.remainder(constant_penalties, BATCH_SIZE)
         similarity_loss = torch.remainder(similarity_loss, BATCH_SIZE)
