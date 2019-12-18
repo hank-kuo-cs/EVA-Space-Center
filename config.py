daa7a7453d110c0563f65e3abdeb262a97550cd4
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-18 21:22:51 +0800

    find defect image

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from net import VGG19, ResNet18, ResNet50
 
 # Basic Setting
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M:%S')
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 IS_PARALLEL = False
 PARALLEL_GPUS = [0, 2, 3]
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
